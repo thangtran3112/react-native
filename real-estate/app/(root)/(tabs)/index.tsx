e414741c2c8ee80100baa4401c13fd34591045e6
Toby Tran <ttran@stackline.com>
2025-09-20 10:32:21 -0700

    nativewind and expo 54 scalfolding

---
+++ real-estate/app/(root)/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text className="text-2xl font-bold my-10">Welcome to restate</Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property 1</Link>
+    </View>
+  );
+}
